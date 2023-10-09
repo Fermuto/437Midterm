c1955ee75644655cbe3a6feb4ef00a9de0967f5c
Alexander Jin <62297015+Fermuto@users.noreply.github.com>
2023-10-09 15:28:44 -0500

    Small fixes, cleanup of toplevel + testbench

--- MidtermTestbench.v
+++ MidtermTestbench.v
@@ -1,46 +1,54 @@
 `timescale 1ns / 1ps
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Description:
+//      437 Top level
+// Dependencies:
+//      ClockGenerator.v
+//      I2CInterface.v
+// Revision:
+//      r0.0.0.2
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 module MidtermTestBench();
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     //Declare wires and registers that will interface with the module under test
     //Registers are initilized to known states. Wires cannot be initilized.
     wire [7:0] led;
-    reg sys_clkn = 1;
-    wire sys_clkp;
+    reg        sys_clkn = 1;
+    wire       sys_clkp;
 
-    reg PCControl;
-    reg [6:0] SlaveAddress, SubAddress;
-    reg ReadWrite;
-    reg [7:0] WriteData, BytesToRead;
+    reg         PCControl;
+    reg  [6:0]  SlaveAddress, SubAddress;
+    reg         ReadWrite;
+    reg  [7:0]  WriteData, BytesToRead;
     wire [31:0] ReadData;
 
-    wire ACK_bit, SCL, SDA;
-    wire [7:0] UpperState, LowerState;
+    wire        ACK_bit, SCL, SDA;
+    wire [7:0]  UpperState, LowerState;
     wire [31:0] Telemetry;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     //Invoke the module that we like to test
-    I2CInterface UUT (.led(led),
-                    .sys_clkn(sys_clkn),
-                    .sys_clkp(sys_clkp),
-                    // .I2C_SCL_0(I2C_SCL_0),
-                    // .I2C_SDA_0(I2C_SDA_0),
-                    .FSM_Clk_reg(FSM_Clk_reg),
-                    .ILA_Clk_reg(ILA_Clk_reg),
-                    .PCControl(PCControl),
-                    .SlaveAddress(SlaveAddress),
-                    .SubAddress(SubAddress),
-                    .ReadWrite(ReadWrite),
-                    .WriteData(WriteData),
-                    .BytesToRead(BytesToRead),
-                    .ReadData(ReadData),
-                    .ACK_bit(ACK_bit),
-                    .SCL(SCL),
-                    .SDA(SDA),
-                    .UpperState(UpperState),
-                    .LowerState(LowerState),
-                    .Telemetry(Telemetry)
-                    );
+    I2CInterface UUT (  .led(led),
+                        .sys_clkn(sys_clkn),
+                        .sys_clkp(sys_clkp),
+                        // .I2C_SCL_0(I2C_SCL_0),
+                        // .I2C_SDA_0(I2C_SDA_0),
+                        .FSM_Clk_reg(FSM_Clk_reg),
+                        .ILA_Clk_reg(ILA_Clk_reg),
+                        .PCControl(PCControl),
+                        .SlaveAddress(SlaveAddress),
+                        .SubAddress(SubAddress),
+                        .ReadWrite(ReadWrite),
+                        .WriteData(WriteData),
+                        .BytesToRead(BytesToRead),
+                        .ReadData(ReadData),
+                        .ACK_bit(ACK_bit),
+                        .SCL(SCL),
+                        .SDA(SDA),
+                        .UpperState(UpperState),
+                        .LowerState(LowerState),
+                        .Telemetry(Telemetry));
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     // Generate a clock signal. The clock will change its state every 5ns.
